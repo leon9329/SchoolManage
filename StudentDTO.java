e90bb706a0faa5d507f6cbea6a4b58e4edb33272
giantObject <msclown1@naver.com>
2018-03-23 20:06:27 +0900

    create StudentDTO.java and ProfessorDTO

---
+++ StudentDTO.java
@@ -0,0 +1,64 @@
+//Student DTO
+public class StudentDTO {
+	private String classOf; // 학번
+	private String name; // 이름
+	private String sex; // 성별
+	private String age; // 나이
+	private String privateCourse; // 좋아하는 과목
+	
+	StudentDTO(){};
+	
+	StudentDTO(String classOf, String name, String sex, String age, String privateCourse) {
+		this.classOf = classOf;
+		this.name = name;
+		this.sex = sex;
+		this.age = age;
+		this.privateCourse = privateCourse;
+
+	}
+	
+	public void StudentInfo() { // 학생 정보 출력
+		
+		System.out.println(classOf + " " + name + " " + sex + " " + age + " " + privateCourse);
+	}
+
+	public void setClassOf(String classOf) {
+		this.classOf = classOf;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public void setPrivateCourse(String privateCourse) {
+		this.privateCourse = privateCourse;
+	}
+
+	public String getClassOf() {
+		return classOf;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public String getPrivateCouse() {
+		return privateCourse;
+	}
+}
